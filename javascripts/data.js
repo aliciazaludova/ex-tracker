1eb5f4460400b9df49b44581e702f1bb11f4f222
Alicia Zaludova <aliciazaludova@gmail.com>
2018-05-05 07:59:22 -0500

    locations printing

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,14 +1,15 @@
 const loadEx = require('./ex');
 const loadLocations = require('./locations');
-const writeEx = require('./dom');
+const showAll = require('./dom');
 
 const whenExLoads = (data) => {  // passing in the word data parses the data here
   console.log('data', data);
-  $('#ex-card').append(writeEx(data.ex)); // this is the new printToDom
+  $('#ex-card').html(showAll.writeEx(data.ex)); // this is the new printToDom // ask for clarification of this statement
 };
 
 const whenLocationsLoad = (data) => { // passing in the word data parses the data here
   console.log('data', data);
+  $('#location-card').append(showAll.writeLocations(data.locations));
 };
 
 const whenStuffDontLoad = (error) => {
